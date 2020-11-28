ead726a55a667bd4095a912694cc227242d0e033
lucasdabout <73880029+lucasdabout@users.noreply.github.com>
2020-11-28 18:45:38 +0100

    photo mentions

--- siteweb-main/php/mentionslegales.php
+++ siteweb-main/php/mentionslegales.php
@@ -2,7 +2,8 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="../lucasdabout.css">
+    <link rel="stylesheet" href="lucasdabout.css">
+    <link rel="icon" href="siteweb-main\images\logo.ico"/>
     <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Allura&display=swap" rel="stylesheet">
     <title>Mentions Légales</title>
@@ -12,7 +13,7 @@
     <div class="mentionsrond">
 
 
-    <img class="imgmentions"src="images/DSC05727.JPG" alt="">
+    <img class="imgmentions"src="C:\Users\Lucas Dabout\Documents\GitHub\lucas\siteweb-main\images\DSC05727.JPG" alt="">
     <div class="textementions">
     <p class="lucasdaboutmentions">Lucas Dabout</p>
     <p class="etudiantmentions">étudiant</p>
