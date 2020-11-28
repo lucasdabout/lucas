91591f12dcc8951bce0934348a7d6c1124bdc639
lucasdabout <73880029+lucasdabout@users.noreply.github.com>
2020-11-28 18:17:43 +0100

    mentions légales et :hover

--- siteweb-main/index.php
+++ siteweb-main/index.php
@@ -5,10 +5,13 @@
     <link rel="stylesheet" href="lucasdabout.css">
     <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Allura&display=swap" rel="stylesheet">
-    <link rel="icon" href="logo.ico"/>
+    <link rel="icon" href="images/logo.ico"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/ff64834a9a.js" crossorigin="anonymous"></script>
      <script src="https://www.google.com/recaptcha/api.js"></script>
      <script>
+
   function onSubmit(token) {
     document.getElementById("demo-form").submit();
   }
@@ -22,6 +25,7 @@
     </div>
     <nav>
       <ol>
+        <li><a class="texte_barre_de_navigation_lucas" href="index.php">Lucas Dabout</a></li>
         <li><a class="texte_barre_de_navigation" href="#apropos">A propos</a></li>
         <li><a class="texte_barre_de_navigation" href="#Compétences">Compétences</a></li>
         <li><a class="texte_barre_de_navigation" href="#Expérience">Expérience</a></li>
@@ -31,44 +35,44 @@
     </nav>
     <p class="LucasDabout">Lucas Dabout</p>
     <p class="Etudiant">étudiant</p><br><br><br><br><br><br><br>
-    <a name="apropos">
+    <a id="apropos">
 
       <?php
-      include 'apropos.php';
+      include 'php/apropos.php';
        ?>
 
     </a>
-    <a name="Compétences">
+    <a id="Compétences">
 
       <?php
-      include 'competences.php';
+      include 'php/competences.php';
        ?>
 
     </a>
-    <a name="Expérience">
+    <a id="Expérience">
 
       <?php
-      include 'experience.php';
+      include 'php/experience.php';
       ?>
 
     </a>
-    <a name="Formation">
+    <a id="Formation">
 
       <?php
-      include 'formation.php';
+      include 'php/formation.php';
       ?>
 
     </a>
-    <a name="contact">
+    <a id="contact">
 
       <?php
-      include 'contact.php';
+      include 'php/contact.php';
        ?>
 
     </a>
-    <a href="index.php"> <i class="fas fa-arrow-circle-up"></a></i>
+    <a href="index.php"> <i class="fas fa-arrow-circle-up"></i></a>
     <footer>
-      Mentions légales Lucas Dabout
+      <a class="liensmentionslegales" href="mentionslegales.php">Mentions légales</a> - © 2020 réalisé par Lucas Dabout - Étudiant - BTS SIO
     </footer>
   </body>
 </html>
