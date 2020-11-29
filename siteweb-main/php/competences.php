08e9e1a58b243628d9e90f9ab5b81aee9e78607f
lucasdabout <73880029+lucasdabout@users.noreply.github.com>
2020-11-29 14:33:54 +0100

    update

--- siteweb-main/php/competences.php
+++ siteweb-main/php/competences.php
@@ -8,6 +8,7 @@
     <p class="pourcent_texte">PHP<br>Python</p>
   </div>
   <div class="progress_bar_oral">
+	<p class="hovervoir">pourcent</p>
     <div class="pourcent_oral"></div>
     <p class="pourcent_texte">Aisance à l'oral<br>Réseau Informatique</p>
   </div><br><br>
