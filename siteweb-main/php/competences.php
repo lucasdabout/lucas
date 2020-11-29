fbb8a0e3cb8d06baba6ab86325c410321cb38895
lucasdabout <73880029+lucasdabout@users.noreply.github.com>
2020-11-29 14:55:40 +0100

    :hover competences

--- siteweb-main/php/competences.php
+++ siteweb-main/php/competences.php
@@ -1,14 +1,16 @@
 <div class="box_compétences">
   <div class="progress_bar_html">
+	<p class="hovervoir">85%</p>
     <div class="pourcent_html"></div>
     <p class="pourcent_texte">HTML<br>CSS</p>
   </div>
   <div class="progress_bar_php">
+	<p class="hovervoir">15%</p>
     <div class="pourcent_php"></div>
     <p class="pourcent_texte">PHP<br>Python</p>
   </div>
   <div class="progress_bar_oral">
-	<p class="hovervoir">pourcent</p>
+	<p class="hovervoir">55%</p>
     <div class="pourcent_oral"></div>
     <p class="pourcent_texte">Aisance à l'oral<br>Réseau Informatique</p>
   </div><br><br>
